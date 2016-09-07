9e4eb841b1517e051dc34d03e3f3729875c3fc8b
Anders Hafreager <andershaf@gmail.com>
2016-09-07 11:25:44 +0200

    Fixed so there is a max limit on plane distances

--- zsm5pg/zsm5geometry.cpp
+++ zsm5pg/zsm5geometry.cpp
@@ -45,9 +45,9 @@ void Zsm5geometry::randomWalkStep(float standardDeviation)
         float dx = Random::nextGaussianf(0, standardDeviation);
         float dy = Random::nextGaussianf(0, standardDeviation);
         float dz = Random::nextGaussianf(0, standardDeviation);
-        if(m_deltaXVector[i] + dx > 2 && Random::nextFloat() < m_randomWalkFraction) m_deltaXVector[i] += dx;
-        if(m_deltaYVector[i] + dy > 2 && Random::nextFloat() < m_randomWalkFraction) m_deltaYVector[i] += dy;
-        if(m_deltaZVector[i] + dz > 2 && Random::nextFloat() < m_randomWalkFraction) m_deltaZVector[i] += dz;
+        if(m_deltaXVector[i] + dx > 2 && m_deltaXVector[i] + dx < 20 && Random::nextFloat() < m_randomWalkFraction) m_deltaXVector[i] += dx;
+        if(m_deltaYVector[i] + dy > 2 && m_deltaYVector[i] + dy < 20 && Random::nextFloat() < m_randomWalkFraction) m_deltaYVector[i] += dy;
+        if(m_deltaZVector[i] + dz > 2 && m_deltaZVector[i] + dz < 20 && Random::nextFloat() < m_randomWalkFraction) m_deltaZVector[i] += dz;
     }
 }
 
